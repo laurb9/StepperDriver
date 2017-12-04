laurb9/StepperDriver

commit db1c1f4be400b48ef287227bba4de0bd747ce735

Timing calculation fixes.
Initialize rest where necessary
Don't call calcStepPulse() in setRPM() / setMicrostep()

src/BasicStepperDriver.cpp
@@ -51,7 +51,6 @@ void BasicStepperDriver::begin(short rpm, short microsteps){
  */
 void BasicStepperDriver::setRPM(short rpm){
     this->rpm = rpm;
-    calcStepPulse();
 }
 
 /*
@@ -65,12 +64,12 @@ short BasicStepperDriver::setMicrostep(short microsteps){
             break;
         }
     }
-    calcStepPulse();
     return this->microsteps;
 }
 
 /*
  * Set speed profile - CONSTANT_SPEED, LINEAR_SPEED (accelerated)
+ * accel and decel are given in [full steps/s^2]
  */
 void BasicStepperDriver::setSpeedProfile(Mode mode, short accel, short decel){
     this->mode = mode;
@@ -152,7 +151,7 @@ long BasicStepperDriver::getTimeForMove(long steps){
             break;
         case CONSTANT_SPEED:
         default:
-            t = step_pulse * steps_remaining;
+            t = STEP_PULSE(rpm, motor_steps, microsteps);
     }
     return t;
 }
@@ -176,11 +175,22 @@ void BasicStepperDriver::startRotate(double deg){
  * calculate the interval til the next pulse
  */
 void BasicStepperDriver::calcStepPulse(void){
-    // feed remainder into successive steps to increase accuracy (Atmel DOC8017)
-    static long rest = 0;
+    // remainder to be fed into successive steps to increase accuracy (Atmel DOC8017)
+    static long rest;
+
+    if (steps_remaining <= 0){  // this should not happen, but avoids strange calculations
+        return;
+    }
+
+    steps_remaining--;
+    step_count++;
+
     // if constant speed
     if (mode == LINEAR_SPEED){
         if (step_count <= steps_to_cruise){
+            if (step_count == 1){     // first step, initialize rest
+                rest = 0;
+            }
             /*
              * accelerating
              */
@@ -212,8 +222,6 @@ long BasicStepperDriver::nextAction(void){
             step_state = HIGH;
         } else {
             step_state = LOW;
-            steps_remaining--;
-            step_count++;
             calcStepPulse();
         }
         digitalWrite(step_pin, step_state);

commit c7beae700d3fe77e68630543ae5859da34bba0b6

getTimeForMove() was returning single step time in constant speed mode
Fixes issue #39
This bug was introduced in commit db1c1f4

src/BasicStepperDriver.cpp
@@ -204,7 +204,7 @@ long BasicStepperDriver::getTimeForMove(long steps){
             break;
         case CONSTANT_SPEED:
         default:
-            t = STEP_PULSE(rpm, motor_steps, microsteps);
+            t = steps * STEP_PULSE(rpm, motor_steps, microsteps);
     }
     return t;
 }